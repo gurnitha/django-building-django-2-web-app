bba1985dffb56b408ef615779b3e930e8dc9b7be
ing <ingafter60@outlook.com>
2021-06-02 11:17:32 +0700

    4. Creating MovieList view

--- app/core/views.py
+++ app/core/views.py
@@ -1,3 +1,13 @@
+# core/views.py
+
+# Django modules
 from django.shortcuts import render
+from django.views.generic import ListView
+
+# Django locals
+from app.core.models import Movie	
 
 # Create your views here.
+
+class MovieList(ListView):
+	model = Movie
